fe0924efdcaa4b624cf5e9031931caa7370297be
Amber Bertucci <amber@akarys.me>
2022-06-22 13:38:18 +0200

    Project setup

--- src/main/java/me/akarys/nochatreporting/mixin/ExampleMixin.java
+++ src/main/java/me/akarys/nochatreporting/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package me.akarys.nochatreporting.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import me.akarys.nochatreporting.NoChatReporting;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		NoChatReporting.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
