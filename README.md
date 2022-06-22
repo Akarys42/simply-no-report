fc6fa64b7ff90193f2e33ef83fbf20f2a5c6cf6f
Amber Bertucci <amber@akarys.me>
2022-06-22 15:01:52 +0200

    Conversion approach

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # No Chat Reporting
+
+This mod gives the option to server admins to disable chat reporting, by converting chat messages to system messages, 
+in a non-intrusive way.
+
+Disabling is done through the `disableChatReporting` gamerule and is set to false by default, to let server admins
+decide whether that's something they want or not.
+
+Note that this mod will probably break other mods that modify chat messages.
--- src/main/java/me/akarys/nochatreporting/NoChatReporting.java
+++ src/main/java/me/akarys/nochatreporting/NoChatReporting.java
@@ -1,21 +1,38 @@
 package me.akarys.nochatreporting;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
+import net.fabricmc.fabric.api.message.v1.ServerMessageEvents;
+import net.minecraft.network.message.MessageType;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
+import net.minecraft.world.GameRules;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class NoChatReporting implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("nochatreporting");
+
+	public static final GameRules.Key<GameRules.BooleanRule> DISABLE_CHAT_REPORTING =
+			GameRuleRegistry.register("disableChatReporting", GameRules.Category.CHAT, GameRuleFactory.createBooleanRule(false));
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		ServerMessageEvents.ALLOW_CHAT_MESSAGE.register((message, sender, typeKey) -> {
+			ServerWorld world = sender.getWorld();
+			if (world.getGameRules().getBoolean(DISABLE_CHAT_REPORTING)) {
+				LOGGER.info("Proxying chat message from {}: {}", sender.getName().getString(), message.raw().getContent().getString());
 
-		LOGGER.info("Hello Fabric world!");
+				MinecraftServer server = sender.getServer();
+				Text formattedMessage = Text.of("<" + sender.getName().getString() + "> " + message.raw().getContent().getString());
+				server.getPlayerManager().broadcast(formattedMessage, MessageType.SYSTEM);
+
+				return false;
+			} else {
+				return true;
+			}
+		});
 	}
 }
